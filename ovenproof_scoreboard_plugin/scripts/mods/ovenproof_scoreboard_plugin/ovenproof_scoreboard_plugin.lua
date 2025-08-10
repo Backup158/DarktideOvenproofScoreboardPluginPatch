e2ae95061007cf96f2b25a2790882f3804f64ed6
LackupL58 <beekoop158+gh@gmail.com>
2025-08-10 00:06:51 -0600

    style: removed debug echos

--- ovenproof_scoreboard_plugin/scripts/mods/ovenproof_scoreboard_plugin/ovenproof_scoreboard_plugin.lua
+++ ovenproof_scoreboard_plugin/scripts/mods/ovenproof_scoreboard_plugin/ovenproof_scoreboard_plugin.lua
@@ -371,7 +371,7 @@ function mod.on_all_mods_loaded()
 	-- ######
 	mod:hook_safe(CLASS.HudElementTacticalOverlay, "_draw_widgets", function(func, self, dt, t, input_service, ui_renderer, render_settings, ...)
 		mod:manage_blank_rows()
-		mod:echo("IF YOU SEE THIS YELL AT ME: tactical overlay widgets")
+		--mod:echo("IF YOU SEE THIS YELL AT ME: tactical overlay widgets")
 		-- base mod hooks onto this first, but executes after the original function
 	end)
 	-- ######
@@ -379,7 +379,7 @@ function mod.on_all_mods_loaded()
 	-- ######
 	mod:hook_safe(CLASS.EndView, "on_enter", function(self)
 		mod:manage_blank_rows()
-		mod:echo("IF YOU SEE THIS YELL AT ME: entering end view")
+		--mod:echo("IF YOU SEE THIS YELL AT ME: entering end view")
 		-- base mod hooks onto this first, but executes after the original function
 	end)
 
