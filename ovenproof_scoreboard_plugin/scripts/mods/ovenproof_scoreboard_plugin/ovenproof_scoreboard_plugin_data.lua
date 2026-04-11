9326dff34e2ffe48cb7ee2f1f9ed4566835e16fa
Backup158 <beekoop158@gmail.com>
2026-04-10 19:42:17 -0500

    fix: reused table by copying so it doesn't relocalize (making the <> issue)

--- ovenproof_scoreboard_plugin/scripts/mods/ovenproof_scoreboard_plugin/ovenproof_scoreboard_plugin_data.lua
+++ ovenproof_scoreboard_plugin/scripts/mods/ovenproof_scoreboard_plugin/ovenproof_scoreboard_plugin_data.lua
@@ -1,7 +1,22 @@
 local mod = get_mod("ovenproof_scoreboard_plugin")
 
+-- ########################
+-- Variables and Performance
+-- ########################
+local table = table
+local table_clone = table.clone
 local data_tables = mod:io_dofile("ovenproof_scoreboard_plugin/scripts/mods/ovenproof_scoreboard_plugin/data_tables")
 
+local offense_tier_options = {
+	{text = "offense_tier_0", value = "offense_tier_0", title = "offense_tier_0", },
+	{text = "offense_tier_1", value = "offense_tier_1", title = "offense_tier_1", },
+	{text = "offense_tier_2", value = "offense_tier_2", title = "offense_tier_2", },
+	{text = "offense_tier_3", value = "offense_tier_3", title = "offense_tier_3", },
+}
+
+-- ########################
+-- Helper Functions
+-- ########################
 -- Creates a widget with a subwidget to toggle it only for Havoc
 local function create_setting_with_havoc_toggle(setting_id_code)
 	return
@@ -37,13 +52,9 @@ for _, state in pairs(mod.optional_states_disabled) do
 	optional_states_disabled_widgets[#optional_states_disabled_widgets + 1] = create_setting_toggle("track_"..state, false)
 end
 
-local offense_tier_options = {
-	{text = "offense_tier_0", value = "offense_tier_0", },
-	{text = "offense_tier_1", value = "offense_tier_1", },
-	{text = "offense_tier_2", value = "offense_tier_2", },
-	{text = "offense_tier_3", value = "offense_tier_3", },
-}
-
+-- ########################
+-- Widgets
+-- ########################
 local localizations = {
 	name = mod:localize("mod_title"),
 	description = mod:localize("mod_description"),
@@ -101,22 +112,22 @@ local localizations = {
 					{	["setting_id"] = "categorize_total_melee",
 						["type"] = "dropdown",
 						["default_value"] = "offense_tier_1",
-						["options"] = offense_tier_options,
+						["options"] = table_clone(offense_tier_options),
 					},
 					{	["setting_id"] = "categorize_total_ranged",
 						["type"] = "dropdown",
 						["default_value"] = "offense_tier_1",
-						["options"] = offense_tier_options,
+						["options"] = table_clone(offense_tier_options),
 					},
 					{	["setting_id"] = "categorize_total_blitz",
 						["type"] = "dropdown",
 						["default_value"] = "offense_tier_1",
-						["options"] = offense_tier_options,
+						["options"] = table_clone(offense_tier_options),
 					},
 					{	["setting_id"] = "categorize_total_companion",
 						["type"] = "dropdown",
 						["default_value"] = "offense_tier_1",
-						["options"] = offense_tier_options,
+						["options"] = table_clone(offense_tier_options),
 					},
 				},
 			},
